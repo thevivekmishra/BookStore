39f85d26aa39d057d783f5285f686abda50bf91f
thevivekmishra <vivekmishra89994@gmail.com>
2024-05-29 23:53:52 +0530

    mernproject

--- Backend/routes/book.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import {getBook} from "../controller/book.controller.js";
-const router = express.Router();
-
-//route
-router.get("/",getBook);
-
-export default router;
